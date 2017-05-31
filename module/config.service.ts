0e99b3f812584fca89a9233d4f1eedc53166c752
GonzalD <d.gonzalez@outlook.fr>
2017-05-31 14:59:39 +0200

    component display simple table with fields : menu, market places, shippingweek

--- module/config.service.ts
+++ module/config.service.ts
@@ -1,13 +1,14 @@
 import { Http, Headers } from '@angular/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/map'
 
 @Injectable()
 export class ConfigService {
 
-  API_SERVER: String;
+  API_SERVER: String = 'http://localhost:4000';
 
-  API_VERSION : String = '/v1';
+  API_VERSION: String = '/v1';
 
   LOG_LEVEL: String = 'debug';
 
@@ -23,63 +24,64 @@ export class ConfigService {
   // disqus:'a0602093a94647cd948e95fadb9b9e38'; /*karibou prod*/
 
   github = {
-      repo:'evaletolab/karibou-doc',
-      token:'7b24b8ec909903ad91d4548fc6025badaf1501bc'
+    repo: 'evaletolab/karibou-doc',
+    token: '7b24b8ec909903ad91d4548fc6025badaf1501bc'
   };
 
   cover: String = '';
   // cover:'img/home-site.jpg';
 
-  postfinance ={
-    url:'https://e-payment.postfinance.ch/ncol/test/orderstandard_utf8.asp'
+  postfinance = {
+    url: 'https://e-payment.postfinance.ch/ncol/test/orderstandard_utf8.asp'
   };
 
   user = {
-    photo:'//placehold.it/80x80',
+    photo: '//placehold.it/80x80',
   };
 
   shop = {
-    photo:{
-      fg:"//placehold.it/400x300",
-      owner:"//placehold.it/80x80&text=owner",
-      bg:''
+    photo: {
+      fg: "//placehold.it/400x300",
+      owner: "//placehold.it/80x80&text=owner",
+      bg: ''
     }
   };
-  loginPath = ['/admin','/account'];
+  loginPath = ['/admin', '/account'];
   readonlyPath = ['/wallet/create'];
-  avoidShopUIIn = ['/admin','/login','/signup','/page']
+  avoidShopUIIn = ['/admin', '/login', '/signup', '/page']
 
   providers = [
-    {name: 'twitter',   url: this.API_SERVER + '/auth/twitter'},
-    {name: 'google+',   url: this.API_SERVER + '/auth/google'},
-    {name: 'persona',   url: this.API_SERVER + '/auth/browserid'},
+    { name: 'twitter', url: this.API_SERVER + '/auth/twitter' },
+    { name: 'google+', url: this.API_SERVER + '/auth/google' },
+    { name: 'persona', url: this.API_SERVER + '/auth/browserid' },
   ];
 
   otherproviders = [
-    {name: 'google+',   url: this.API_SERVER + '/auth/google'},
-    {name: 'facebook',  url: this.API_SERVER + '/auth/facebook'},
-    {name: 'linkedin',  url: this.API_SERVER + '/auth/linkedin'},
-    {name: 'github',    url: this.API_SERVER + '/auth/github'}
+    { name: 'google+', url: this.API_SERVER + '/auth/google' },
+    { name: 'facebook', url: this.API_SERVER + '/auth/facebook' },
+    { name: 'linkedin', url: this.API_SERVER + '/auth/linkedin' },
+    { name: 'github', url: this.API_SERVER + '/auth/github' }
   ];
 
-  private headers:Headers;
-  private config:Observable<any>;
+  private headers: Headers;
+  private config: Observable<any>;
 
   constructor(
     public http: Http
   ) {
     this.headers = new Headers();
-    this.headers.append('Content-Type', 'application/json');    
-    this.config = this.http.get(this.API_SERVER+'/v1/config?lang=fr',{
-      headers:this.headers, 
+    this.headers.append('Content-Type', 'application/json');
+    this.config = this.http.get(this.API_SERVER + '/v1/config?lang=', {
+      headers: this.headers,
       withCredentials: true,
     })
-        .map(res => {
-          Object.assign(this.shop,res.json())
-        });
+      .map(res => {
+        Object.assign(this.shop, res.json());
+        return res.json();
+      });
   }
 
-  getConfig():Observable<any>{
-    return this.config.subscribe(res => console.log(res));
+  getConfig(): Observable<any> {
+    return this.config;
   }
 }
--- src/app/config.component.ts
+++ src/app/config.component.ts
@@ -1,16 +1,66 @@
-    import { Component }    from '@angular/core';
-    import { Observable }   from 'rxjs/Observable';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
-    import { ConfigService }    from '../../module/config.service';
-    
-    @Component({
-      selector: 'my-config',
-      template: `
-        <h1>Affichage de la config serveur</h1>
-        
+import { ConfigService } from '../../module/config.service';
+
+@Component({
+    selector: 'my-config',
+    template: `
+        <h2>Affichage de la config serveur</h2>
+        <div *ngIf="configObject">
+            <table>
+                <tr>
+                    <th>Menu</th>
+                </tr>
+                <tr *ngFor="let sub of configObject.menu">
+                    <td>{{sub.name.fr}} </td>
+                </tr>
+            </table> <br>
+            <table>
+                <tr>
+                    <th>Market places</th>
+                    <th>lat/lng</th>
+                </tr>
+                
+                <tr *ngFor="let sub of configObject.marketplace.list">
+                    <td>{{sub.name}} </td>
+                    <td>{{sub.lat}}/{{sub.lng}}</td>
+                </tr>
+            </table><br>
+            <table>
+                <tr>
+                    <th>Shipping Week</th>
+                </tr>
+                <tr *ngFor="let sub of configObject.shippingweek">
+                    <td>{{sub | date:'medium'}} </td>
+                </tr>
+            </table>
+        </div>
         `,
-        providers: [ ConfigService ]
-    })
-    export class ConfigComponent {
-      
+    styles: [`
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+`],
+    providers: [ConfigService]
+})
+export class ConfigComponent {
+
+    private configObject;
+
+    constructor(private config: ConfigService) { }
+
+    ngOnInit() { this.getConfig(); }
+
+    getConfig() {
+        this.config.getConfig()
+            .subscribe(
+            res => {
+                console.log(res);
+                this.configObject = res;
+            }
+            );
     }
+
+}
