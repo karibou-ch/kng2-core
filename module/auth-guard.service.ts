6860fba334bada0e8502bbe012a8c18c7c3b1c85
GonzalD <d.gonzalez@outlook.fr>
2017-06-20 00:20:41 +0200

    fix routing auth-guard not working when refreshing the page

--- module/auth-guard.service.ts
+++ module/auth-guard.service.ts
@@ -1,6 +1,7 @@
 import { CanActivate }    from '@angular/router';
 import { Injectable } from '@angular/core';
 import { LoaderService } from './loader.service'
+import { Observable } from "rxjs/Rx";
 import { Router } from '@angular/router';
 import { User } from './user.service'
 
@@ -16,13 +17,19 @@ export class AuthGuardService implements CanActivate {
 
   }
 
-  canActivate() {
-    this.loaderSrv.ready().subscribe(
-      (loader) => {
+  //this prevent user from entering dashboard component when not logged
+  //use Observable to prevent error when refreshing the page
+  //see issue : https://stackoverflow.com/questions/42677274/angular-2-route-guard-not-working-on-browser-refresh/42678548
+  canActivate(): Observable<boolean> {
+    return this.loaderSrv.ready()
+      .take(1)
+      .map((loader) => {
         Object.assign(this.user, loader[1]);
+        return this.user.isAuthenticated();
+      })
+      .do((auth) => {
+        if(!auth) this._router.navigate(['/login'])
       });
-      if(!this.user.isAuthenticated()) this._router.navigate(['/login']);
-      return this.user.isAuthenticated();
   }
 
 }
