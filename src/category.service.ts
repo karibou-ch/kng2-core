0281ad390e8150d27fdfdaa34be5d8844a0aa593
evaletolab <evaleto@gmail.com>
2017-06-29 08:50:48 +0200

    add replaysubject

--- src/category.service.ts
+++ src/category.service.ts
@@ -1,12 +1,8 @@
 import { Http, Headers, RequestOptions } from '@angular/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Rx';
+import { Observable, BehaviorSubject, ReplaySubject } from 'rxjs/Rx';
 import { ConfigService } from './config.service'
 
-
-export interface QueryFilter {
-  stats?: boolean;
-}
 
 export class Category {
   slug: string;
@@ -25,7 +21,10 @@ export class Category {
 
 @Injectable()
 export class CategoryService {
-  // TODO make observable content !!
+  //
+  // common multicast to update UX when one shop on the list is modified
+  public  category$: ReplaySubject<Category>;   
+
   config:any;
 
   private defaultCategory = {
@@ -41,33 +40,6 @@ export class CategoryService {
     map: Map<string, Category>; //key is a slug
   }
 
-
-  private updateCache(cat: Category) {
-    if (this.cache.map[cat.slug])
-      return Object.assign(this.cache.map[cat.slug], cat);
-  }
-
-  private deleteCache(cat: Category) {
-    if (this.cache.map[cat.slug]) {
-      this.cache.map.delete(cat.slug);
-      let index = this.cache.list.indexOf(cat)
-      if (index > -1)
-        this.cache.list.splice(index, 1);
-    }
-  }
-
-  private addCache(category:Category){
-    //
-    //check if already exist on cache and add in it if not the case
-    if (!this.cache.map[category.slug]){
-      this.cache.map[category.slug] = category;
-      this.cache.list.push(category);
-      return;
-    }
-    //update existing entry
-    return Object.assign(this.cache.map[category.slug],category);    
-  }
-
   private headers: Headers;
 
   constructor(
@@ -79,6 +51,28 @@ export class CategoryService {
     this.headers.append('Content-Type', 'application/json');
     this.config = configSrv.config;
   }
+
+  private deleteCache(cat: Category) {
+    if (this.cache.map[cat.slug]) {
+      this.cache.map.delete(cat.slug);
+      let index = this.cache.list.indexOf(cat)
+      if (index > -1)
+        this.cache.list.splice(index, 1);
+    }
+  }
+
+  private updateCache(category:Category){
+    //
+    //check if already exist on cache and add in it if not the case
+    if (!this.cache.map[category.slug]){
+      this.cache.map[category.slug] = category;
+      this.cache.list.push(category);
+      return;
+    }
+    //update existing entry
+    return Object.assign(this.cache.map[category.slug],category);    
+  }
+
 
   getCurrent() {
     throw new Error("Not implemented");
@@ -111,7 +105,7 @@ export class CategoryService {
       withCredentials: true
     })
       .map(res => res.json() as Category[])
-      .map(categories => categories.map(category=>this.addCache(category)));
+      .map(categories => categories.map(category=>this.updateCache(category)));
     //.catch;
   }
 
@@ -130,7 +124,8 @@ export class CategoryService {
       withCredentials: true
     })
       .map(res => res.json() as Category)
-      .map(category => this.addCache(category))
+      .map(category => this.updateCache(category))
+      .do(this.category$.next)      
       .catch(this.handleError);
 
   }
@@ -145,6 +140,8 @@ export class CategoryService {
       withCredentials: true
     })
     .map(res => res.json() as Category)
+    .map(category => this.updateCache(category))
+    .do(this.category$.next)      
     .catch(this.handleError);
 
   }
@@ -157,7 +154,8 @@ export class CategoryService {
       withCredentials: true
     })
     .map(res => res.json() as Category)
-    .map(category => this.addCache(category))
+    .map(category => this.updateCache(category))
+    .do(this.category$.next)      
     .catch(this.handleError);
     /*
     if (!err) { err = function () { }; }
@@ -177,23 +175,25 @@ export class CategoryService {
       password:password
     })
     .map(res => res.json() as Category)
+    .do(this.category$.next)      
     .map(cat => this.deleteCache(cat))
     .catch(this.handleError);
   }
 
 
   private handleError (error: Response | any) {
-  // In a real world app, you might use a remote logging infrastructure
-  let errMsg: string;
-  if (error instanceof Response) {
-    const body = error.json() || '';
-    const err = JSON.stringify(body);
-    errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
-  } else {
-    errMsg = error.message ? error.message : error.toString();
+    //
+    // In a real world app, you might use a remote logging infrastructure
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
   }
-  console.error(errMsg);
-  return Observable.throw(errMsg);
-}
 
 }
