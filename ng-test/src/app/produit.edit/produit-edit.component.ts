9795e9756c2754875ef95dbf825abd892a75e57d
doudiny <yann.doudin@gmail.com>
2017-08-23 15:56:15 +0200

    start of the product edit

---
+++ ng-test/src/app/produit.edit/produit-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-produit-edit',
+  templateUrl: './produit-edit.component.html',
+  styleUrls: ['./produit-edit.component.scss']
+})
+export class ProduitEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
