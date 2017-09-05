64b906ab1af17704d1b3fe91f89439647c98262e
doudiny <yann.doudin@gmail.com>
2017-09-05 13:58:10 +0200

    Update category-edit.component.ts

--- ng-test/src/app/category.edit/category-edit.component.ts
+++ ng-test/src/app/category.edit/category-edit.component.ts
@@ -56,7 +56,6 @@ export class CategoryEditComponent implements OnInit {
 
       //
       // TODO manage on Error user feedback!
-        console.log(this.$category.findBySlug(this.slug))
       this.$category.findBySlug(this.slug).subscribe(cat=>this.category=cat)
 
     });
