4fb630b2f34af83ffc3318f5c66e160eb225bd90
doudiny <yann.doudin@gmail.com>
2017-09-08 11:41:20 +0200

    test

--- pwa/src/app/app.routes.ts
+++ pwa/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuardService, Kng2CoreModule } from '../../../dist';
 
+import { LoginComponent } from './user.login/login.component';
+
 
 export const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
   {
     path: '',
     redirectTo: '/',
