74a13c2e00bd0a9ad8cbbc1bc23923fa3713d3a7
VallatMa <vallat.matthieu@gmail.com>
2017-09-12 16:07:22 +0200

    shop add cards layout

--- pwa/src/app/app.module.ts
+++ pwa/src/app/app.module.ts
@@ -8,12 +8,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './user.login/login.component';
 import { RegisterComponent } from './user.register/register.component';
 
+import { ShopComponent } from './shop/shop.component';
+
 import { AuthGuardService, Kng2CoreModule } from '../../../dist';
 
 import { appRoutes } from './app.routes';
 
-import {MdButtonModule, MdCheckboxModule, MdSidenavModule, MdMenuModule, MdTabsModule,
-        MdToolbarModule, MdIconModule, MdCardModule, MdInputModule} from '@angular/material';
+import {MdButtonModule, MdCheckboxModule, MdSidenavModule, MdMenuModule, MdTabsModule, MdChipsModule,
+        MdToolbarModule, MdIconModule, MdCardModule, MdInputModule, MdGridListModule, MdExpansionModule} from '@angular/material';
+import { FlexLayoutModule } from "@angular/flex-layout";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
@@ -21,17 +24,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    ShopComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     Kng2CoreModule,
     HttpClientModule,
-    MdButtonModule, MdCheckboxModule, MdSidenavModule, MdMenuModule, MdTabsModule,
-    MdToolbarModule ,MdIconModule, MdCardModule, MdInputModule,
+    MdButtonModule, MdCheckboxModule, MdSidenavModule, MdMenuModule, MdTabsModule, MdChipsModule,
+    MdToolbarModule ,MdIconModule, MdCardModule, MdInputModule, MdGridListModule, MdExpansionModule,
     BrowserAnimationsModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- pwa/src/app/app.routes.ts
+++ pwa/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { AuthGuardService, Kng2CoreModule } from '../../../dist';
 
 import { LoginComponent } from './user.login/login.component';
 import { RegisterComponent } from './user.register/register.component';
+import { ShopComponent } from './shop/shop.component';
 
 
 export const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'shop/:slug', component: ShopComponent },
   {
     path: '',
     redirectTo: '/',
