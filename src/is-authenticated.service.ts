f2cd23437feef8da13d70d93c92de74de6265285
olivier <olivier@evaletolab.ch>
2018-03-04 12:22:14 +0100

    base implementation of Auth Guard services

--- src/is-authenticated.service.ts
+++ src/is-authenticated.service.ts
@@ -1,17 +1,13 @@
 import { Injectable } from '@angular/core';
 import { LoaderService } from './loader.service'
-import { Observable } from "rxjs/Rx";
+import { Observable } from "rxjs/Observable";
 import { User, UserService } from './user.service'
 
 @Injectable()
 export class IsAuthenticated {
-
-
   constructor(
     private $user:UserService
-    ) {
-
-  }
+    ) {}
 
   //this prevent user from entering dashboard component when not logged
   //use Observable to prevent error when refreshing the page
@@ -19,5 +15,29 @@ export class IsAuthenticated {
   canActivate() {
     return this.$user.currentUser.isAuthenticated();
   }
-
 }
+
+
+@Injectable()
+export class IsAdmin {
+  constructor(
+    private $user:UserService
+    ) {}
+
+  canActivate() {
+    return this.$user.currentUser.isAdmin();
+  }
+}
+
+// https://angular.io/api/router/CanActivate
+// @Injectable()
+// class CanActivateTeam implements CanActivate {
+//   constructor(private permissions: Permissions, private currentUser: UserToken) {}
+
+//   canActivate(
+//     route: ActivatedRouteSnapshot,
+//     state: RouterStateSnapshot
+//   ): Observable<boolean>|Promise<boolean>|boolean {
+//     return this.permissions.canActivate(this.currentUser, route.params.id);
+//   }
+// }
