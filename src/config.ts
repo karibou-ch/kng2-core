804e075de38ffbdc828dcc8f04a23d83b86eefe1
olivier <olivier@evaletolab.ch>
2021-01-16 17:42:50 +0100

    chg: clean code

--- src/config.ts
+++ src/config.ts
@@ -1,34 +1,34 @@
 
 
 
-export class Config{
-  constructor(){
-    this.shared={};
-    this.isAvailable=false;
+export class Config {
+  constructor() {
+    this.shared = {};
+    this.isAvailable = false;
   }
-  isAvailable:boolean;
-  shared:any;
+  isAvailable: boolean;
+  shared: any;
 
-  API_SERVER:string;
-  API_VERSION:string;
+  API_SERVER: string;
+  API_VERSION: string;
 
-  LOG_LEVEL:string;
+  LOG_LEVEL: string;
 
-  AUTH_SUCCESS_REDIRECT_URL:string;
-  AUTH_ERROR_REDIRECT_URL:string;
+  AUTH_SUCCESS_REDIRECT_URL: string;
+  AUTH_ERROR_REDIRECT_URL: string;
 
-  user:any;
-  loader:string[];
+  user: any;
+  loader: string[];
 
-  loginPath:string[];
-  readonlyPath:string[];
-  avoidShopUIIn:string[];
+  loginPath: string[];
+  readonlyPath: string[];
+  avoidShopUIIn: string[];
 
   //
   // Compute the next potential shipping day.
   // It depends on the hours needed to harvest/prepare a placed order
   potentialShippingDay(): Date {
-    let now = new Date(),
+    const now = new Date(),
         potential = new Date(now.getTime() + 3600000 * (this.shared.hub.timelimit));
 
     //
@@ -52,22 +52,22 @@ export class Config{
   //
   // Compute the next potential shipping days in one week.
   potentialShippingWeek() {
-    let potential = this.potentialShippingDay();
+    const potential = this.potentialShippingDay();
     return potential.dayToDates(
       config.shared.hub.weekdays
     );
   }
 
   getShippingWeek() {
-    let oneWeek=['Di','Lu','Ma','Me','Je','Ve','Sa'], today=new Date();
+    const oneWeek = ['Di', 'Lu', 'Ma', 'Me', 'Je', 'Ve', 'Sa'], today = new Date();
 
     //
     // all the week is off!
-    if(!this.shared.order){
-      return oneWeek.map((day,i)=>{return {label:day,state:'disabled'}});
+    if (!this.shared.order) {
+      return oneWeek.map((day, i) => ({label:day,state:'disabled'}));
     }
 
-    return oneWeek.map((day,i) => {
+    return oneWeek.map((day, i) => {
       return (this.shared.hub.weekdays.indexOf(i) > -1) ?
         {label: day, state: ''} : { label: day, state: 'disabled'};
     });
@@ -89,22 +89,33 @@ export class Config{
 
   }
 
-  getShippingDays():Date[]{
-    return this.shared.shippingweek||[];
+  getShippingDays(): Date[] {
+    return this.shared.shippingweek || [];
   }
+
+  getShippingDistrict(postalCode) {
+    const districts = Object.keys(this.shared.shipping.district);
+    const district = districts.find( district => {
+      return this.shared.shipping.district[district] &&
+             this.shared.shipping.district[district].indexOf(postalCode) !== -1;
+    });
+    // FIXME hypercenter should not be default
+    return district || 'hypercenter';
+  }
+
 }
 
 
-export interface ConfigMenu{
-  name:{
-    en:string;
-    fr:string;
-    de:string;
+export interface ConfigMenu {
+  name: {
+    en: string;
+    fr: string;
+    de: string;
   };
-  url:string;
-  weight:number;
-  group:string;
-  active:boolean
+  url: string;
+  weight: number;
+  group: string;
+  active: boolean;
 }
 
 /**
@@ -116,8 +127,8 @@ export interface ConfigMenu{
  * }
  */
 
-export enum ConfigKeyStoreEnum{
+export enum ConfigKeyStoreEnum {
   KIO2_LOGIN_REMEMBER   = 0,
   KIO2_SERVER           = 1
 }
-export var config:Config = new Config();
+export let config: Config = new Config();
