ae95f59ecfcefee8e3c8e1ce2ece2bb6592e4910
olivier <olivier@evaletolab.ch>
2022-09-05 10:55:39 +0200

    new: initial port for a11

--- src/index.ts
+++ src/index.ts
@@ -69,9 +69,10 @@ export { EnumCancelReason,
 export { Utils } from './util';
 //
 // directives & pipes
+export { MarkdownDirective } from './util.markdown.directive';
 export { bgSrcDirective } from './util.bg-src.directive';
 export { ConfirmDeleteDirective } from './util.confirm-delete.directive';
-export { OrderPortionPipe, OrderBasepricePipe } from './order/order.pipe.portion';
+export { OrderPortionPipe, OrderBasepricePipe, OrderBasepricePipeEx } from './order/order.pipe.portion';
 
 //
 // export variable instance
