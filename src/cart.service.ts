db8a9804c812d162c70e9816e5cf5fb4f9bfa398
olivier <olivier@evaletolab.ch>
2022-09-07 15:41:58 +0200

    fix: current cart date can be null

--- src/cart.service.ts
+++ src/cart.service.ts
@@ -700,9 +700,10 @@ export class CartService {
   hasPotentialShippingReductionMultipleOrder(): boolean {
     //
     // check if there is a pending order for this address
-    if(this.currentPendingOrder && this.currentPendingOrder.shipping){
+    if(this.currentPendingOrder && 
+       this.currentPendingOrder.shipping){
       const whenDay = this.currentPendingOrder.shipping.when.getDate();
-      const nextDay = this.cache.currentShippingDay.getDate();
+      const nextDay = this.cache.currentShippingDay && this.cache.currentShippingDay.getDate();
       return (whenDay == nextDay );
     }
     return false;
@@ -831,10 +832,8 @@ export class CartService {
 
       //
       // get state from pending order
-      // FIXME set address equals to pending
+      // FIXME set address and payment in setContext fonction
       if(this.currentPendingOrder){
-        const issuer = this.currentPendingOrder.payment.issuer;
-        this.cache.payment = new UserCard(this.currentUser.payments.find(payment => payment.issuer == issuer));  
         this.cache.address = UserAddress.from(this.currentPendingOrder.shipping);          
       }else
 
@@ -1153,6 +1152,32 @@ export class CartService {
 
     this.currentShops = shops || this.currentShops || [];
     Object.assign(this.currentUser, user);
+
+
+    //
+    // if there is an open order, 
+    // - set the default address
+    // - set the default date
+    orders = (orders||[]).sort((a,b)=> b.oid-a.oid);
+    if(orders.length) {
+      // we can use issuer when the user is set after edition
+      const issuer = orders[0].payment.issuer;
+      this.cache.payment = user.payments.find(payment => payment.issuer == issuer) || this.cache.payment;
+      this.cache.address = UserAddress.from(orders[0].shipping);          
+
+      const potentialDate = new Date(orders[0].shipping.when);
+      const defaultNext = Order.nextShippingDay(user,config.shared.hub);
+      if(potentialDate>=defaultNext){
+        this.cache.currentShippingDay = potentialDate;
+      }
+
+    }
+    //
+    // EnumFinancialStatus[EnumFinancialStatus.authorized]
+    let open = orders.find(order => order.payment.status == 'authorized' && !order.shipping.parent); 
+    if(open) {
+      this.currentPendingOrder = open;
+    }    
     //
     // avoid multiple reset on same context
     if(this.currentHub == config.shared.hub.slug) {
@@ -1166,31 +1191,6 @@ export class CartService {
     //
     // set default shipping address
     this.cache.address = user.getDefaultAddress();
-    // FIXME default shipping 
-    // this.cache.currentShippingDay = Order.nextShippingDay(user);
-    // this.cache.currentShippingTime = 16;
-
-    //
-    // use default date when hub.carts.items > 0
-
-
-    //
-    // if there is an open order, 
-    // - set the default address
-    // - set the default date
-    orders = (orders||[]).sort((a,b)=> b.oid-a.oid);
-    if(orders.length) {
-      const alias = orders[0].payment.alias;
-      this.cache.currentShippingDay = new Date(orders[0].shipping.when);
-      this.cache.payment = user.payments.find(payment => payment.alias == alias) || this.cache.payment;
-      this.cache.address = UserAddress.from(orders[0].shipping);          
-    }
-    //
-    // EnumFinancialStatus[EnumFinancialStatus.authorized]
-    let opens = orders.filter(order => order.payment.status == 'authorized' && !order.shipping.parent); 
-    if(opens.length) {
-      this.currentPendingOrder = opens[0];
-    }
 
     //
     // mark cart ready
