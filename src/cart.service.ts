7d3720f7ae87235913102b423aff68734e697a14
olivier <olivier@evaletolab.ch>
2023-04-04 12:59:32 +0200

    fix: address is not always an instance of class

--- src/cart.service.ts
+++ src/cart.service.ts
@@ -1122,7 +1122,7 @@ export class CartService {
     //
     // check if address exist before to save it
     if(!deposit){
-      address = this.currentUser.addresses.find(add => add.isEqual(address)) || new UserAddress();
+      address = this.currentUser.addresses.find(add => address.isEqual(add)) || new UserAddress();
     } else {
       address = deposit;
     }
