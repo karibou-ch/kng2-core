e7def2f0d356780a0a2b6004338eb4373faf87fa
olivier <olivier@evaletolab.ch>
2023-08-08 13:18:45 +0200

    new: add dbl click detection

--- src/util.one-click.directive.ts
+++ src/util.one-click.directive.ts
@@ -2,15 +2,20 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 
 @Directive({
-    selector: '[one-click]'
+  selector: '[one-click]'
 })
 export class OneClickDirective {
-    @Input() debounceTime = 500;
+  @Input() debounceTime = 500;
 
-    constructor(private elementRef: ElementRef) { }
-    @HostListener('click', ['$event'])
-    clickEvent() {
-        this.elementRef.nativeElement.setAttribute('disabled', 'true');
-        setTimeout(() => this.elementRef?.nativeElement?.removeAttribute('disabled'), this.debounceTime);
-    }
+  constructor(private $elem: ElementRef) { }
+  @HostListener('click', ['$event'])
+  clickEvent() {
+    const $ref = this.$elem;
+    $ref.nativeElement.setAttribute('disabled', 'true');
+    setTimeout(() => {
+      try {
+        $ref.nativeElement.removeAttribute('disabled');
+      }catch(err) {}
+    }, this.debounceTime);
+  }
 }
